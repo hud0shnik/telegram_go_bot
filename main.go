840ed6ceb54bcff1b0ad791f6e085c20e544e9d5
Danila Egorov <c4nsero@mail.ru>
2021-12-10 15:09:35 +0300

    update /time and /ip
    Сделал подсказку для пользователей, убрал лишнюю проверку

--- main.go
+++ main.go
@@ -100,8 +100,11 @@ func respond(botUrl string, update mods.Update) error {
 		case "/start", "/help":
 			mods.Help(botUrl, update)
 			return nil
-		case "/time", "какой сегодня день?", "сколько времени?":
+		case "/time", "какой сегодня день?":
 			mods.SendTime(botUrl, update, DanyaFlag)
+			return nil
+		case "/ip":
+			mods.SendMsg(botUrl, update, "Чтобы узнать страну по ip, отправьте: \n\n/ip 67.77.77.7")
 			return nil
 		case "owo":
 			mods.SendMsg(botUrl, update, "UwU")
